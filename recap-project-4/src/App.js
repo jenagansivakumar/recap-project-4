120540584e4d8181965962a4e983d5a64897c240
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-04-18 16:36:33 +0200

    added buttons/inputs

--- recap-project-4/src/App.js
+++ recap-project-4/src/App.js
@@ -1,8 +1,38 @@
-import React from "react";
-import "./App.css";
+import { useState } from "react";
 
-function App() {
-  return <h1 className="heading"> Weather & Activities App </h1>;
+export default function App({ onAddActivity = () => {} }) {
+  const [activity, setActivity] = useState("");
+  const [isGoodWeather, setIsGoodWeather] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onAddActivity({ activity, isGoodWeather });
+    setActivity("");
+    setIsGoodWeather(false);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Add Activity</h2>
+      <label>
+        Name of Activity:
+        <input
+          type="text"
+          value={activity}
+          onChange={(e) => setActivity(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Good Weather Activity:
+        <input
+          type="checkbox"
+          checked={isGoodWeather}
+          onChange={(e) => setIsGoodWeather(e.target.checked)}
+        />
+      </label>
+      <br />
+      <button type="submit">Add</button>
+    </form>
+  );
 }
-
-export default App;
